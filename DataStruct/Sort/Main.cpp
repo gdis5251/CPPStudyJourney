8b14d819c15eaf324077839775fa65031cbdcd67
gdis5251 <841784596@qq.com>
2019-04-28 17:08:50 +0800

    [Gerald][write][2019/4/28] Add Select Sort.

--- DataStruct/Sort/Main.cpp
+++ DataStruct/Sort/Main.cpp
@@ -1,14 +1,17 @@
 #include "InsertingSort.h"
 #include "PrintArr.h"
 #include "ShellSort.h"
+#include "SelectSort.h"
 
 int main(void)
 {
     std::vector<int> arr = {4, 5, 1, 9, 7, 5, 2, 8};
-   // InsertingSort(arr);
-   // PrintArray(arr);
-   // InsertingSortDown(arr);
-    ShellSort(arr);    
+    //InsertingSort(arr);
+    //PrintArray(arr);
+    //InsertingSortDown(arr);
+    //ShellSort(arr);    
+    //SelectSort(arr);
+    SelectSortOP(arr);
     PrintArray(arr);
 
     return 0;
