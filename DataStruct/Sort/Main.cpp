bf8ea6e581826524d8654b58b00866e4791ccd55
gdis5251 <841784596@qq.com>
2019-04-28 16:28:34 +0800

    [Gerald][write][2019/4/28] add Shell Sort

--- DataStruct/Sort/Main.cpp
+++ DataStruct/Sort/Main.cpp
@@ -1,12 +1,14 @@
 #include "InsertingSort.h"
 #include "PrintArr.h"
+#include "ShellSort.h"
 
 int main(void)
 {
     std::vector<int> arr = {4, 5, 1, 9, 7, 5, 2, 8};
-    InsertingSort(arr);
-    PrintArray(arr);
-    InsertingSortDown(arr);
+   // InsertingSort(arr);
+   // PrintArray(arr);
+   // InsertingSortDown(arr);
+    ShellSort(arr);    
     PrintArray(arr);
 
     return 0;
