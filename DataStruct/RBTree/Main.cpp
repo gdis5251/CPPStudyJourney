bf02b0acaf3797b5206b59c18e0dce69568df556
Gerald Kwok <841784596@qq.com>
2019-06-02 12:17:03 +0800

    [Gerld][write][2019/6/2] 完成红黑树

--- DataStruct/RBTree/Main.cpp
+++ DataStruct/RBTree/Main.cpp
@@ -1,16 +1,29 @@
 #include "RBTree.hpp"
+#include <time.h>
 
 void TestRBTree()
 {
     RBTree<int, int> rb;
+#if 0
     int arr[] = {4, 5, 2, 6, 7, 8, 9, 1, 3};
 
     for (const auto& e : arr)
     {
         rb.Insert(std::make_pair(e, e));
     }
+#endif
+
+	srand(time(nullptr));
+	int num = (rand() % 100);
+	for (int i = 0; i < num; i++)
+	{
+		int input = rand();
+		rb.Insert(std::make_pair(input, input));
+	}
 
     rb.Inorder();
+	if (rb.IsValidRBTree())
+		std::cout << "ÊÇÒ»¿ÅºìºÚÊ÷ £¡" << std::endl;
 }
 
 int main(void)
--- DataStruct/RBTree/RBTree.hpp
+++ DataStruct/RBTree/RBTree.hpp
@@ -37,6 +37,7 @@ public:
 
     bool Insert (const std::pair<K, V>& kv)
     {
+		// 首先判断是否是一颗空树
         if (_header->_pParent == nullptr)
         {
             pNode root = new Node;
@@ -152,13 +153,13 @@ public:
                     break;
                 }
             }
-            // 红黑树根始终是黑色的
-            _header->_pParent->_color = BLACK;
-
-            // 为了以后实现迭代器
-            _header->_pLeft = LeftMost();
-            _header->_pRight = RightMost();
         }
+		// 红黑树根始终是黑色的
+		_header->_pParent->_color = BLACK;
+
+		// 为了以后实现迭代器
+		_header->_pLeft = LeftMost();
+		_header->_pRight = RightMost();
     }
 
     void Inorder()
@@ -167,9 +168,67 @@ public:
         std::cout << std::endl;
     }
 
+	bool IsValidRBTree()
+	{
+		pNode pRoot = _header->_pParent;
+
+		// 空树也是红黑树
+		if (nullptr == pRoot)
+			return true;
+		// 检测根节点是否满足情况
+		if (BLACK != pRoot->_color)
+		{
+			std::cout << "违反红黑树性质二：根节点必须为黑色" << std::endl;
+			return false;
+		}
+		// 获取任意一条路径中黑色节点的个数
+		size_t blackCount = 0;
+		pNode pCur = pRoot;
+		while (pCur)
+		{
+			if (BLACK == pCur->_color)
+				blackCount++;
+			pCur = pCur->_pLeft;
+		}
+		// 检测是否满足红黑树的性质，k用来记录路径中黑色节点的个数
+		size_t k = 0;
+		return _IsValidRBTree(pRoot, k, blackCount);
+	}
+	
+
 private:
+	bool _IsValidRBTree(pNode pRoot, size_t k, const size_t blackCount)
+	{
+		//走到null之后，判断k和black是否相等
+		if (nullptr == pRoot)
+		{
+			if (k != blackCount)
+			{
+				std::cout << "违反性质四：每条路径中黑色节点的个数必须相同" << std::endl;
+				return false;
+			}
+			return true;
+		}
+
+		// 统计黑色节点的个数
+		if (BLACK == pRoot->_color)
+			k++;
+		// 检测当前节点与其双亲是否都为红色
+		pNode pParent = pRoot->_pParent;
+		if (pParent && RED == pParent->_color && RED == pRoot->_color)
+		{
+			std::cout << "违反性质三：没有连在一起的红色节点" << std::endl;
+			return false;
+		}
+		return _IsValidRBTree(pRoot->_pLeft, k, blackCount) &&
+			_IsValidRBTree(pRoot->_pRight, k, blackCount);
+	}
+
     void _Inorder(pNode& root)
     {
+		if (root == nullptr)
+			return;
+
         _Inorder(root->_pLeft);
         std::cout << root->_kv.second << " ";
         _Inorder(root->_pRight);
