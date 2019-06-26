176badd2bdb07dade14edc12e99f6d3817967c05
gdis5251 <841784596@qq.com>
2019-06-26 12:13:49 +0800

    [Gerald][update][2019/6/26] modify code. if the number will be insert greater than vector's size will return.

--- DataStruct/BitSet/Main.cpp
+++ DataStruct/BitSet/Main.cpp
@@ -4,16 +4,16 @@ int main()
 {
     BitSet bitset(100);
 
-    bitset.set(5);
-    bitset.set(8);
-    bitset.set(99);
+    // bitset.set(5);
+    // bitset.set(8);
+    bitset.set(20000);
 
-    std::cout << bitset.test(5) << std::endl;
-    std::cout << bitset.test(8) << std::endl;
-    std::cout << bitset.test(99) << std::endl;
+    // std::cout << bitset.test(5) << std::endl;
+    // std::cout << bitset.test(8) << std::endl;
+    std::cout << bitset.test(20000) << std::endl;
 
-    bitset.reset(8);
-    std::cout << bitset.test(8) << std::endl;
+    // bitset.reset(8);
+    // std::cout << bitset.test(8) << std::endl;
 
     return 0;
 }
