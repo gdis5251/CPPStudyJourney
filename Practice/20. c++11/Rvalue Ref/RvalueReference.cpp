1bf2d1781779e65aba7226e2c6551e72cc603b42
gdis5251 <841784596@qq.com>
2019-07-06 15:18:29 +0800

    [Gerald][update][2019/7/6] perfect forward

--- Practice/20. c++11/Rvalue Ref/RvalueReference.cpp
+++ Practice/20. c++11/Rvalue Ref/RvalueReference.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 
+#if 0
 class String
 {
 public:
@@ -69,7 +70,30 @@ String GetString(const char* str)
 int main(void)
 {
     String str = GetString("Hello");
-    String str2(GetString("World"));
+    String str2(str);
+    String str3(std::move(str));
 
+    return 0;
+}
+#endif
+
+using namespace std;
+
+void Fun(int &x) { cout << "lvalue ref" << endl; }
+void Fun(int &&x) { cout << "rvalue ref" << endl; }
+void Fun(const int &x) { cout << "const lvalue ref" << endl; }
+void Fun(const int &&x) { cout << "const rvalue ref" << endl; }
+template <typename T>
+void PerfectForward(T &&t) { Fun(std::forward<T>(t)); }
+
+int main()
+{
+    PerfectForward(10); // rvalue ref
+    int a;
+    PerfectForward(a);            // lvalue ref
+    PerfectForward(std::move(a)); // rvalue ref
+    const int b = 8;
+    PerfectForward(b);            // const lvalue ref
+    PerfectForward(std::move(b)); // const rvalue ref
     return 0;
 }
